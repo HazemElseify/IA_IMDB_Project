9b5190d0ebda94e600e85aeb8e144079430c58fb
HazemElseify <hazem.hassan.11.hh@gmail.com>
2022-04-22 01:56:55 +0200

    static admin module

---
+++ IA_Project/IA_Project/Controllers/adminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IA_Project.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: admin
+        public ActionResult homemovie()
+        {
+            return View();
+        }
+        public ActionResult homeactor()
+        {
+            return View();
+        }
+        public ActionResult homedirector()
+        {
+            return View();
+        }
+    }
+}
